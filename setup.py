82bf329d69e5232e74f522c4781cdbf38c8e9be4
Sneha Singhania <sneha.a@iiitb.org>
2018-04-03 07:48:23 +0530

    added setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='crunchbasescraper',
     version='0.1.0',
     author='Sneha Singhania',
     author_email='sneha.a@iiitb.org',
-    scripts=['crunchbase.py', 'database_orm.py', 'get_crunchbase_data.py', 'simple_scraper.py', 're_demo.py', 'database_json.py'],
+    scripts=['crunchbase.py', 'database_orm.py', 'simple_scraper.py', 're_demo.py', 'database_json.py'],
     license='MIT',
     description='Useful to scrape data from Crunchbase and storing it in a database',
     long_description=open('README.md').read(),
+    python_requires='>=3.5',
     install_requires=[
         "requests >= 2.7.0",
         "sqlalchemy == 1.2.6",
